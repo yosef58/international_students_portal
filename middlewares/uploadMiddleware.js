cd47afa347fd5697b4875d6ea4402339fe69cb13
unknown <youssef>
2026-05-04 06:00:30 +0300

    fix uploaders

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -4,7 +4,7 @@ import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import path from 'path';
 
 const getCloudinary = () => {
-  cloudinary.config({
+ await cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
