160c7fc2fa0ec882c2224fc061122997095ffa36
unknown <youssef>
2026-05-04 05:20:46 +0300

    fix uploaders

--- middlewares/asyncwrapper.js
+++ middlewares/asyncwrapper.js
@@ -1,6 +1,9 @@
-const asyncwrapper =(asyncfn)=>{
-    return (req,res,next)=>{
-        asyncfn(req,res,next).catch(next);
+const asyncwrapper = (asyncfn) => {
+    return (req, res, next) => {
+        asyncfn(req, res, next).catch((err) => {
+            console.error('ERROR:', err.message, err.stack); // 👈 add this
+            next(err);
+        });
     }
 }
 export default asyncwrapper;
