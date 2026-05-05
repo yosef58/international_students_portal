a9a4177434c92d3b337f4b248f0fb4bc73574b57
unknown <youssef>
2026-05-05 11:23:45 +0300

    add frontend url

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://internationalstudentsportal-production-5e18.up.railway.app"
+      "https://international-student-bortal.netlify.app"
     ],
     credentials: true
   })
