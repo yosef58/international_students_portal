f03063e30191dadfa8391a72c774a252010aa408
unknown <youssef>
2026-05-11 00:27:56 +0300

    use resend onboard email

--- utils/emailService.js
+++ utils/emailService.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendNotificationEmail = async ({ to, subject, message }) => {
   try {
     await resend.emails.send({
-      from: 'https://international-student-bortal.netlify.app',
+      from: 'onboarding@resend.dev',
       to,
       subject,
       html: `
